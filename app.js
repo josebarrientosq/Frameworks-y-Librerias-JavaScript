384b67597575bf1f09f7f0efa384ec6e66c2fd4c
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-02-24 02:34:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,105 @@ var fila5=[0,0,0,0,0,0,0]
 var fila6=[0,0,0,0,0,0,0]
 var fila7=[0,0,0,0,0,0,0]
 var matriz=[fila1,fila2,fila3,fila4,fila5,fila6,fila7]
-var k=0
+var fila1b=[0,0,0,0,0,0,0]
+var fila2b=[0,0,0,0,0,0,0]
+var fila3b=[0,0,0,0,0,0,0]
+var fila4b=[0,0,0,0,0,0,0]
+var fila5b=[0,0,0,0,0,0,0]
+var fila6b=[0,0,0,0,0,0,0]
+var fila7b=[0,0,0,0,0,0,0]
+var matriz2=[fila1b,fila2b,fila3b,fila4b,fila5b,fila6b,fila7b]
+
+
+var i=0;j=0;k=0;z=0;
+var x1=0;y1=0;x2=0;y2=0;pos1=0;pos2=0; 
+
 
 function numero(obj){
 	return obj.src.substring(obj.src.length-5,obj.src.length-4)
 	
 }
 
+function aparece(elemento){
+	$(elemento).animate(
+	{
+		left: "+=100"
+	}, 500, function(){
+		desaparece(elemento)
+	}
+
+	)
+}
+function desaparece(elemento){
+	$(elemento).animate(
+	{
+		left: "-=100"
+	}, 500, function(){
+		aparece(elemento)
+	}
+
+	)
+}
+
+function arriba(elemento){
+  $(elemento).animate(
+    {
+      top: "-=10"
+    }, 500, function(){
+      abajo(elemento)
+    }
+  )
+}
+
+function abajo(elemento){
+  $(elemento).animate(
+    {
+      top: "+=10"
+    }, 500, function(){
+      arriba(elemento)
+    }
+  )
+}
+
+
+function mostrar(){
+	$( ".caramelo" ).detach();
+	//for (j=0 ;j<7 ;j++)
+	//	$(".col-"+(j+1)).empty()
+
+	for (i=0 ;i<7;i++)
+			for (j=0 ;j<7 ;j++){
+				$(".col-"+(j+1)).append("<img src='image/"+ matriz[i][j]+".png' class='caramelo'/>");
+				console.log(matriz[i][j])
+			}
+
+			$(".caramelo").draggable();
+			arriba($(".caramelo"))
+
+}
+
+function borrar(){
+	$( ".caramelo" ).detach();
+}
+
 $(function(){
 
+
+	for (i=0 ;i<7;i++)
+			for (j=0 ;j<7 ;j++){
+				matriz[i][j]=Math.floor(Math.random()*3+1)
+				matriz2[i][j]=matriz[i][j]
+				$(".col-"+(j+1)).append("<img src='image/"+ matriz[i][j]+".png' class='caramelo'/>");
+
+		}
+		
+		//mostrar()
+		$(".caramelo").draggable();
+
+
+
+
+/*
 	$('.parpadeo').each(function() {
     var elem = $(this);
     setInterval(function() {
@@ -28,7 +118,7 @@ $(function(){
         }    
     }, 500);
 });
-
+*/
 
 
 	$(".btn-reinicio").click(function(){
@@ -42,12 +132,7 @@ $(function(){
 			//	console.log($(".col-"+j+">.caramelo:nth-child("+i+")").attr("src").substring(6,7)  )
 				matriz[i-1][j-1]=$(".col-"+j+">.caramelo:nth-child("+i+")").attr("src").substring(6,7)
 */
-		for (var i=0 ;i<7;i++)
-			for (var j=0 ;j<7 ;j++){
-				matriz[i][j]=Math.floor(Math.random()*3+1)
-				$(".col-"+(j+1)).append("<img src='image/"+ matriz[i][j]+".png' class='caramelo'/>");
-
-		}
+		borrar()
 
     	
 
@@ -55,8 +140,8 @@ $(function(){
 
   	
   $(".btn-reinicio2").click(function(){
-  		for (var i=0 ;i<7 ;i++)
-			for (var j=0 ;j<7 ;j++){
+  		for (i=0 ;i<7 ;i++)
+			for (j=0 ;j<7 ;j++){
 				k=0
 				while(matriz[i][j+k]==matriz[i][j+k+1] && j+k+1<7){
 					k++
@@ -66,43 +151,78 @@ $(function(){
 					for(var z=0 ; z<=k; z++)
 						matriz[i][j+z]=9
 				}
-				//console.log(matriz[i][j])
+				console.log(matriz[i][j])
 			}
 
-		for (var i=1 ;i<=7 ;i++)
-			for (var j=1 ;j<=7 ;j++){
-				if(matriz[i-1][j-1]==9){
 
-					$(".col-"+j+">.caramelo:nth-child("+i+")").toggle("bounce",30000)
+		for (j=0 ;j<7 ;j++)
+			for (i=0 ;i<5 ;i++){
+				k=0
+
+				while( matriz2[i+k][j]==matriz2[i+k+1][j]&&(i+k+1)<7){
+					k++
+					if(i+k+1==7) break;
 				}
-				console.log(matriz[i-1][j-1])
+				
+				if(k>=2){
+					for(var z=0 ; z<=k; z++)
+						matriz2[i+z][j]=9
+				}
+				console.log(matriz[i][j])
+			}
+
+		for (j=0 ;j<7 ;j++)
+			for (i=0 ;i<7 ;i++){
+				if(matriz[i][j]==9 || matriz2[i][j]==9)
+					matriz[i][j]=9
 			}
 
 
 
+		for (i=1 ;i<=7 ;i++)
+			for (j=1 ;j<=7 ;j++){
+				if(matriz[i-1][j-1]==9){
+					arriba($(".col-"+j+">.caramelo:nth-child("+i+")"))
+				}
+				//console.log(matriz[i-1][j-1])
+				//console.log($(".col-"+j+">.caramelo:nth-child("+i+")"))
+			}
   });
+
+  
 	//for (var i=1 ;i<8 ;i++)
 	//	for (var j=1 ;j<8 ;j++){
 	//		$(".col-"+i).append("<img src='image/"+ Math.floor(Math.random()*3+1)+".png' class='caramelo'/>");
 			//$("<img src='image/"+ Math.floor(Math.random()*3+1)+".png' class='caramelo'/>").appendTo(".col-"+i); 
 	//	}
 
-		$(".caramelo").draggable();
+		
 	//	$(".caramelo").droppable({accept: ".caramelo" });
 
  	$(".caramelo").mousedown(function(){
  		ficha1=this
  	//	temporal=ficha1.clone();
-  		//console.log(numero(ficha1))
+  		//console.log($(ficha1).parent())
+  		pos1=$(".caramelo" ).index( this )
+  		x1=Math.floor(pos1/7)
+  		y1=pos1-7*x1
+  		console.log("pos= "+pos1+" col= "+ x1+" fila= "+y1 );
   		//$(this).toggle();  
   	}); 
 
-  	$(".caramelo").mouseup(function(){
-  		ficha2=this
+  $(".caramelo").mouseup(function(){
+  		pos2=$(".caramelo" ).index( this )
+  		x2=Math.floor(pos2/7)
+  		y2=pos2-7*x2
+  		console.log("pos= "+pos2+" col= "+ x2+" fila= "+ y2);
+  		var temp= matriz[y2][x2]
+  		matriz[y2][x2]=matriz[y1][x1]
+  		matriz[y1][x1]=temp
   		
+  		mostrar()
   		
-  		ficha2.after(ficha1);
-  		$(".caramelo").css( "top", "0" );
+  	//	ficha2.after(ficha1);
+  	//	$(".caramelo").css( "top", "0" );
   		//ficha1.css( "top", "0" )
 
   	//	ficha2.replaceWith(temporal)
